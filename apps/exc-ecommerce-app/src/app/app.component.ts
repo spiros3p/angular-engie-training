c2f74c93d500ff2db68482c90fa730f520e90dee
Spyridon Peppas <spyridonpeppas@Spyridons-MacBook-Pro.local>
2022-09-25 11:27:13 +0200

    Initial commit

---
+++ apps/exc-ecommerce-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-engie-training-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'exc-ecommerce-app';
+}
